88670526a32a7706b3c6e82d0c5374a20f441510
rosielockett95 <rosie.lockett4@gmail.com>
2026-01-30 12:41:32 +0000

    backend url updated for render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://rococo-malasada-715229.netlify.app",
+    ],
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
   }),
 );
