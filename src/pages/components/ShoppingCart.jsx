d720283ab1d6d2cf8616cceeb796b3119b95871f
rosielockett95 <rosie.lockett4@gmail.com>
2026-02-02 10:41:11 +0000

    cart functionality and cart total added

--- src/pages/components/ShoppingCart.jsx
+++ src/pages/components/ShoppingCart.jsx
@@ -1,15 +1,37 @@
 import { useCart } from "./CartContext";
+import { useEffect } from "react";
 
 export default function ShoppingCart({ isOpen }) {
   const { cartItems, clearCart, increaseItemQuantity, decreaseItemQuantity } =
     useCart();
   const amountOfItems = Array.isArray(cartItems) ? cartItems.length : 0;
 
+  //  Loops through each instance of that item in the array and adds the first argument onto it
+  const totalCost = cartItems.reduce(
+    (total, item) => total + item.cost * item.quantity,
+    0,
+  );
+
+  const formattedTotal = totalCost.toLocaleString("en-GB", {
+    style: "currency",
+    currency: "GBP",
+  });
+
+  // Loops through each instance of that item in the array and adds the first argument onto it
+  // const totalItems = cartItems.reduce(
+  //   (name, item) => name + item.name,
+  //   "Rosie",
+  // );
+
   return (
     <div className={isOpen ? "shopping-cart-container" : "none"}>
       <div className="shopping-header">
-        <h1>Cart ({amountOfItems})</h1>
-        <button onClick={() => clearCart()}>Remove All</button>
+        <div>
+          <h1>Cart ({amountOfItems})</h1>
+        </div>
+        <div>
+          <button onClick={() => clearCart()}>Remove All</button>
+        </div>
       </div>
 
       {cartItems && cartItems.length > 0 ? (
@@ -34,6 +56,12 @@ export default function ShoppingCart({ isOpen }) {
       ) : (
         <p>Your cart is empty</p>
       )}
+      <div className="total-cost-container">
+        <div>
+          <p className="total-text">Total</p>
+        </div>
+        <div className="total-cost">{formattedTotal}</div>
+      </div>
     </div>
   );
 }
